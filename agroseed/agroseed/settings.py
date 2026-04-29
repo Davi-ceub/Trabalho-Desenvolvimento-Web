058de7a0d787824c8358628cb2cd7ff3288300e4
Davi Castilho <davi.lc@sempreceub.com>
2026-04-29 07:55:10 -0300

    Update settings.py

--- agroseed/agroseed/settings.py
+++ agroseed/agroseed/settings.py
@@ -2,3 +2,14 @@ INSTALLED_APPS = [
     'rest_framework',
     'core',
 ]
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'Agronomia',
+        'USER': 'root',
+        'PASSWORD': 'senha',
+        'HOST': 'localhost',
+        'PORT': '3306',
+    }
+}
